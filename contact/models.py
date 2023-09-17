a49d7a327cb4beef7aad62df4f89e4a357c65bd7
Gabriel Pereira <gabrieldasilvapp@gmail.com>
2023-09-17 18:43:54 -0300

    Model Meta options, verbose_name e verbose_name_plural no Django

--- contact/models.py
+++ contact/models.py
@@ -4,6 +4,10 @@ from django.utils import timezone
 
 # Create your models here.
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
